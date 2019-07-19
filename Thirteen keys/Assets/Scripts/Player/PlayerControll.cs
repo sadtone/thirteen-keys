0da28ec38f79301fa7500bfab2dde318a4b259ef
Sungjin01 <43747041+Sungjin01@users.noreply.github.com>
2019-07-19 21:02:30 +0900

    animator

--- Thirteen keys/Assets/Scripts/Player/PlayerControll.cs
+++ Thirteen keys/Assets/Scripts/Player/PlayerControll.cs
@@ -7,6 +7,7 @@ public class PlayerControll : MonoBehaviour
     private Rigidbody2D rigid2d;
     private PlayerFoot foot;
     private DoorDetect doorDetect;
+    private Animator anim;
     private bool doubleJump;
     private bool isDoor;
 
@@ -21,14 +22,25 @@ public class PlayerControll : MonoBehaviour
         doorDetect = GetComponentInChildren<DoorDetect>();
         rigid2d = GetComponent<Rigidbody2D>();
         foot = GetComponentInChildren<PlayerFoot>();
+        anim = GetComponentInChildren<Animator>();
     }
 
 
     void FixedUpdate()
     {
         v = Input.GetAxisRaw("Vertical");
+        Vector2 velocity = rigid2d.velocity;
 
-        if((Input.GetKey(KeyCode.Space))){
+        if (velocity.y > 1)
+            anim.SetInteger("isJumpUp", 1);
+        else if (velocity.y < 1)
+            anim.SetInteger("isJumpUp", 2);
+            
+        if(foot.isGround)
+            anim.SetInteger("isJumpUp", 0);
+
+
+        if ((Input.GetKey(KeyCode.Space))){
             
             if (foot.isGround)
             {
