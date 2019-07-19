41e8ddb84c084c5cb4823d6aba0df2ffc70e6d8d
Sungjin01 <43747041+Sungjin01@users.noreply.github.com>
2019-07-19 16:04:08 +0900

    Character
    Jump, Double Jump, Slide

---
+++ Thirteen keys/Assets/Scripts/PlayerControll.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControll : MonoBehaviour
+{
+    private Rigidbody2D rigid2d;
+    private PlayerFoot foot;
+    private bool doubleJump;
+
+    public Transform body;
+    public float v;
+    public float jumpPower;
+
+
+    void Start()
+    {
+        rigid2d = GetComponent<Rigidbody2D>();
+        foot = GetComponentInChildren<PlayerFoot>();
+    }
+
+
+    void FixedUpdate()
+    {
+        v = Input.GetAxisRaw("Vertical");
+
+        if((Input.GetKey(KeyCode.Space))){
+            
+            if (foot.isGround)
+            {
+                StartCoroutine(Jump(false));
+            }
+            else
+            {
+                if(foot.groundCnt > 0)
+                {
+                    StartCoroutine(Jump(true));
+                }
+            }
+        }
+        if(v == -1)
+        {
+            if (foot.isGround)
+                Slide(foot.isGround);
+            else
+                Slide(false);
+        }
+        else
+        {
+            Slide(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Door"))
+        {
+            Time.timeScale = 0.2f; //5초에 trigger raius만큼 움직이게 해야해요 아시겠죠
+        }
+    }
+
+    private IEnumerator Jump(bool isDouble)
+    {
+        if (doubleJump)
+            yield break;
+
+        if (isDouble)
+        {
+            foot.groundCnt = 0;   
+        }
+        else
+        {
+            doubleJump = true;
+            foot.groundCnt = 1;
+        }
+
+        rigid2d.velocity = Vector2.zero;
+        rigid2d.AddForce(Vector2.up * jumpPower);
+
+        yield return new WaitForSeconds(0.2f);
+        doubleJump = false;
+        yield break;
+    }
+
+    private void Slide(bool isGround)
+    {
+        if (isGround)
+            body.rotation = Quaternion.Euler(new Vector3(0, 0, 90));
+        else
+            body.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+    }
+}
---
+++ Thirteen keys/Assets/Scripts/PlayerFoot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFoot : MonoBehaviour
+{
+    public bool isGround;
+    public int groundCnt;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            isGround = true;
+            groundCnt = 2;
+        }
+    }
+    
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Ground"))
+            isGround = false;
+    }
+}
